2162ee71343d103e8398cd9fa97c8fe3cbedb56b
Bz14 <bezueyerusalem@gmail.com>
2024-10-11 09:45:52 +0300

    Files updated.

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,11 +1,12 @@
 """A module to implement make_multiplier """
-from typing import Callable
+from typing import Union, Tuple
 
 
-def make_multiplier(multiplier: float) -> Callable:
+def make_multiplier(multiplier: float) -> callable:
     """ A function that returns a function multiplier """
     def multiply(n: float) -> float:
         """ A function that multiplies a float """
         return n * multiplier
-
     return multiply
+
+
