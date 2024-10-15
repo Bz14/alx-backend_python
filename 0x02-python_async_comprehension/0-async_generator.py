1bb9cabfe3d74dbbe2f46cc0db1c394f1ba6aa16
Bz14 <bezueyerusalem@gmail.com>
2024-10-15 12:08:23 +0300

    Files updated.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ Async Generator module """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator() -> float:
+async def async_generator() -> AsyncGenerator[float, None]:
     """ Coroutine that loops 10 times, yields a random number """
     for _ in range(10):
         await asyncio.sleep(1)
