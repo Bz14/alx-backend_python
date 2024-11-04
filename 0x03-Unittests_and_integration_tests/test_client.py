700e8ef5c9743d3e837bf2e5ea8976305e38673b
Bz14 <bezueyerusalem@gmail.com>
2024-11-04 22:01:57 +0300

    Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,10 +27,30 @@ class TestGithubOrgClient(unittest.TestCase):
             mock.return_value = {'repos_url': 'http://example.com'}
             self.assertEqual(test_org._public_repos_url, 'http://example.com')
 
-    def test_public_repos(self):
-        """ TestGithubOrgClient.public_repos method """
+    @patch('client.get_json')
+    def test_public_repos(self, mock_json):
+        """Test TestGithubOrgClient.test_public_repos
+        return the correct value
+        """
+        payloads = [{"name": "google"}, {"name": "Twitter"}]
+        mock_json.return_value = payloads
+
+        with patch('client.GithubOrgClient._public_repos_url') as mock_public:
+            mock_public.return_value = "hey there!"
+            test_class = GithubOrgClient('test')
+            result = test_class.public_repos()
+
+            expected = [p["name"] for p in payloads]
+            self.assertEqual(result, expected)
+
+            mock_json.called_with_once()
+            mock_public.called_with_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ TestGithubOrgClient.has_license method """
         test_org = GithubOrgClient('google')
-        with patch('client.GithubOrgClient.repos_payload') as mock:
-            mock.return_value = [{'name': 'repo1'}, {'name': 'repo2'}]
-            self.assertEqual(test_org.public_repos(), ['repo1', 'repo2'])
-            self.assertEqual(test_org.public_repos('MIT'), ['repo1', 'repo2'])
+        self.assertEqual(test_org.has_license(repo, license_key), expected)
